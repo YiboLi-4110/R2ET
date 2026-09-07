b7763c1dd371a4318edc59d0150de39e9a5ce54b
YiboLi-4110 <Yibo_Li142857@outlook.com>
2026-09-07 12:22:03 +0800

    simple branch

--- datasets/data_preprocess.sh
+++ datasets/data_preprocess.sh
@@ -1,7 +1,8 @@
 BLENDER=/home/lutianyi/Softwares/blender-4.5.3-linux-x64/blender
+# BLENDER=path_to_blender/blender-4.5.3-linux-x64/blender
 
 $BLENDER -b -P ./fbx2bvh_smal33.py -- \
-  --data_path ./shepherd/smal@shepherd/train_char \
+  --data_path ./shepherd/smal@shepherd_dog/train_char \
   --max_cpu_cores 9 \
   --workers 3 \
   --threads_per_worker 1 \
@@ -9,40 +10,39 @@ $BLENDER -b -P ./fbx2bvh_smal33.py -- \
   --mocap_y_up \
 
 python ./preprocess_q_smal33.py \
-  --data_path ./shepherd/smal@shepherd/train_char \
-  --save_path ./shepherd/smal@shepherd/train_q \
+  --data_path ./shepherd/smal@shepherd_dog/train_char \
+  --save_path ./shepherd/smal@shepherd_dog/train_q \
   --overwrite_existing \
   --axis_transform shepherd_y_z_x \
   --forward_mode body \
 
 $BLENDER -b -P ./extract_shape_smal33.py -- \
-  --fbx_root ./shepherd/smal@shepherd/train_char \
-  --save_path ./shepherd/smal@shepherd/train_shape \
+  --fbx_root ./shepherd/smal@shepherd_dog/train_char \
+  --save_path ./shepherd/smal@shepherd_dog/train_shape \
   --one_npz_per_subdirectory \
   --overwrite_existing \
   --axis_transform shepherd_y_z_x \
   --mocap_y_up \
 
 $BLENDER -b -P ./fbx2bvh_smal33.py -- \
-    --data_path ./shepherd/batch3_dogs/batch3_dogs_char \
-    --overwrite_existing \
-    --max_cpu_cores 9 \
-    --workers 3 \
-    --threads_per_worker 1 \
-    --force_rest_pose \
-    --mocap_y_up \
+  --data_path ./shepherd/smal@shepherd_cat/train_char \
+  --max_cpu_cores 9 \
+  --workers 3 \
+  --threads_per_worker 1 \
+  --overwrite_existing \
+  --mocap_y_up \
 
 python ./preprocess_q_smal33.py \
-  --data_path ./shepherd/batch3_dogs/batch3_dogs_char \
-  --save_path ./shepherd/batch3_dogs/batch3_dogs_q \
+  --data_path ./shepherd/smal@shepherd_cat/train_char \
+  --save_path ./shepherd/smal@shepherd_cat/train_q \
   --overwrite_existing \
   --axis_transform shepherd_y_z_x \
   --forward_mode body \
 
 $BLENDER -b -P ./extract_shape_smal33.py -- \
-  --fbx_root ./shepherd/batch3_dogs/batch3_dogs_char \
-  --save_path ./shepherd/batch3_dogs/batch3_dogs_shape \
+  --fbx_root ./shepherd/smal@shepherd_cat/train_char \
+  --save_path ./shepherd/smal@shepherd_cat/train_shape \
+  --one_npz_per_subdirectory \
   --overwrite_existing \
-  --all_fbx_per_subdirectory \
   --axis_transform shepherd_y_z_x \
   --mocap_y_up \
