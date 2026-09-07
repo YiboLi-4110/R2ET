09cf67d6ade67fef4c663e8d4ead06de90005730
YiboLi-4110 <Yibo_Li142857@outlook.com>
2026-09-07 17:20:10 +0800

    simple 17:20

--- retarget.sh
+++ retarget.sh
@@ -1,5 +1,7 @@
 python visualization/batch_r2et_dog_blend.py \
   --config config/visualization_blend_per_dog_smal33.yaml \
-  --gpus 0 \
-  --blender /home/lutianyi/Softwares/blender-4.5.3-linux-x64/blender \
-  --blend_output_dir visualization/blend_retarget_results \
+  --blend_output_dir visualization/blend_retarget_dogs \
+
+python visualization/batch_r2et_dog_blend.py \
+  --config config/visualization_blend_per_cat_smal33.yaml \
+  --blend_output_dir visualization/blend_retarget_cats \
--- visualization.sh
+++ visualization.sh
@@ -1,12 +1,9 @@
-# --blender可以指定自己的blender安装路径
+# 使用--blender可以指定自己的blender安装路径
 python visualization/batch_arp_sequence_smal33.py \
   --config config/visualization_arp_sequence_smal33_dog.yaml \
-  --retarget_mode direct \
   --show_source
 
-# --blender可以指定自己的blender安装路径
+# 使用--blender可以指定自己的blender安装路径
 python visualization/batch_arp_sequence_smal33.py \
   --config config/visualization_arp_sequence_smal33_cat.yaml \
-  --retarget_mode arp \
-  --arp_addon_modules auto_rig_pro-master \
   --show_source
